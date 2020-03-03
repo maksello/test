3e874f6413af80f676d6fbb9d028e52c540b77bb
Maksym Snihur <MSnihur@lan.ad>
2020-03-03 11:11:56 +0200

    4

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,4 @@ function printLabel(labeledObj: LabeledValue) {
 let myObj = { size: 10, label: "Size 10 Object" };
 printLabel(myObj)
 printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
-printLabel(myObj)
+p
