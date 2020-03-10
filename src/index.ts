6154aec8717506acd06e2755ac5ffbbcd3d538fd
Maksym Snihur <MSnihur@lan.ad>
2020-03-10 14:20:30 +0200

    al

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,76 @@
-init();
 var arr1: string[] = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 var arr2: string[] = ['3', '4', '5', '6', '7', '8', '9', 'D', 'J', 'Q', 'K', 'A'];
 var arrCards: string[] = [];
-var result = 0
-var resultPlayer = 0
-var resultPC = 0
+var result = 0;
+var resultPlayer = 0;
+var resultPC = 0;
 var activePlayer = 0;
-var totalPlayer = 0
-var totalPC = 0
+var totalPlayer = 0;
+var totalPC = 0;
 
-function getCard(): string {
-
-    let randSuit: string = arr1[Math.floor(Math.random() * 4)];
-    let randCard: string = arr2[Math.floor(Math.random() * 12)];
-    let randomCard: string = randCard + randSuit;
-
-    console.log(randomCard)
-    if (arrCards.includes(randomCard)) {
-        getCard();
-    } else {
-        sumCards(randomCard)
-        refreshSum()
-
-        // console.log(randomCard)
-        //console.log(arrCards.length)
-        arrCards.push(randomCard);
-        createCardPlace(randomCard);
+class Card {
+    randCard: string;
+    randSuit: string;
+    constructor() {
+        this.randSuit = arr1[Math.floor(Math.random() * 4)];
+        this.randCard = arr2[Math.floor(Math.random() * 12)];
     }
-    return randomCard;
+    includesCard(card: object) {
+        if (arrCards.includes(this.randCard + this.randSuit)) {
+            play();
+        } else {
+            arrCards.push(this.randCard + this.randSuit)
+            if (+this.randCard > 2 && +this.randCard < 10) {
+                result += +this.randCard;
+            } else if (this.randCard === 'D' || this.randCard === 'J' || this.randCard === 'Q' || this.randCard === 'K') {
+                result += 10;
+            } else {
+                result += 11;
+            }
+            // console.log(result)
+            // console.log(arrCards)
+        }
+
+    }
+    includesCardPlace(card: object): void {
+        let placeDiv = document.getElementById('main')
+        let place = window.document.createElement('img')
+        place.src = 'img/kor.jpg'
+        place.id = 'card'
+        place.style.marginRight = '10px'
+        place.style.width = '75px'
+        place.style.height = '150px'
+        placeDiv.appendChild(place)
+    }
+    refreshSum() {
+        if (activePlayer === 0) {
+            resultPlayer = result
+            window.document.getElementById('sumPlayer').innerHTML = `${resultPlayer}`
+        } else {
+            resultPC = result
+            window.document.getElementById('sumPC').innerHTML = `${resultPC}`
+        }
+    }
 }
 
-function getCardPC() {
+function play(): void {
+    var c = new Card();
+    console.log(c)
+    c.includesCard(c);
+    c.includesCardPlace(c)
+    c.refreshSum();
+}
+
+function playPC(): void {
     activePlayer = 1;
     result = 0
     while (resultPC < 14) {
-        getCard()
+        play();
     }
-
+    getWinner();
+    refreshTotal();
+}
+function getWinner() {
     if (resultPlayer > resultPC && resultPlayer < 22 || resultPlayer < 22 && resultPC > 21) {
         totalPlayer++;
         window.document.getElementById('winner').innerHTML = 'PLayer WIN!'
@@ -48,84 +82,12 @@ function getCardPC() {
     } else {
 
     }
-    refreshTotal()
-}
-
-function createCardPlace(card: string): void {
-    let placeDiv = document.getElementById('main')
-    let place = window.document.createElement('img')
-    place.src = '/img/kor.jpg'
-    place.id = 'card'
-    place.style.marginRight = '10px'
-    place.style.width = '75px'
-    place.style.height = '150px'
-    placeDiv.appendChild(place)
 
 }
-//createCardPlace(getCard());
-function sumCards(card: string): void {
-    switch (card[0]) {
-        case '4':
-            result += 4
-            break;
-        case '3':
-            result += 3
-            break;
-        case '5':
-            result += 5
-            break;
-        case '6':
-            result += 6
-            break;
-        case '7':
-            result += 7
-            break;
-        case '8':
-            result += 8
-            break;
-        case '9':
-            result += 9
-            break;
-        case 'D':
-        case 'J':
-        case 'Q':
-        case 'K':
-            result += 10
-            break;
-        case 'A':
-            result += 11
-            break;
-        default:
-            break;
-    }
-
-}
-
-let cardButton = document.querySelector('#btnCard');
-cardButton.addEventListener('click', getCard);
-
-let btnStand = document.querySelector('#btnStand');
-btnStand.addEventListener('click', getCardPC)
-
-let btnNewGame = document.querySelector('#btnNewGame');
-btnNewGame.addEventListener('click', init)
-
-function refreshSum() {
-    if (activePlayer === 0) {
-        resultPlayer = result
-        window.document.getElementById('sumPlayer').innerHTML = `${resultPlayer}`
-    } else {
-        resultPC = result
-        window.document.getElementById('sumPC').innerHTML = `${resultPC}`
-    }
-
-}
-
 function refreshTotal() {
     window.document.getElementById('sumPlayerAll').innerHTML = `${totalPlayer}`
     window.document.getElementById('sumPCAll').innerHTML = `${totalPC}`
 }
-
 function init(): void {
     document.getElementById('main').innerText = ``
     arrCards = [];
@@ -138,3 +100,10 @@ function init(): void {
     window.document.getElementById('winner').innerHTML = ``
 
 }
+
+let cardButton = document.querySelector('#btnCard');
+cardButton.addEventListener('click', play);
+let btnStand = document.querySelector('#btnStand');
+btnStand.addEventListener('click', playPC)
+let btnNewGame = document.querySelector('#btnNewGame');
+btnNewGame.addEventListener('click', init)
